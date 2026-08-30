932debee61ca8fbd398aa2e6964fe97b9b44f1f4
IgnusVanMule <IgnusVM@Hotmail.com>
2026-08-30 02:24:22 -0500

    Remove a stray test script committed by accident

--- f-tmp.ts
+++
@@ -1,6 +0,0 @@
-import "dotenv/config";
-import { PrismaClient } from "./src/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-const p = new PrismaClient({ adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL! }) });
-p.user.findFirst({ where: { email: { contains: "steven" } }, select: { isOrgAdmin: true, isDirector: true } })
- .then(u => { console.log("flags:", JSON.stringify(u)); return p.$disconnect(); });
